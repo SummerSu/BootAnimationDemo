5650b3dfe894392f989c269ee5fb7014913d1a4d
ss <1274005014@qq.com>
2016-10-25 18:35:53 +0800

    Initial Commit

---
+++ BootAnimationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BootAnimationDemo
+//
+//  Created by ss on 2016/10/25.
+//  Copyright © 2016年 ss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
